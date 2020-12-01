7920c5f6539af213b9a9b3c3a68ac5d8b998b29d
Dez <dez@dezs-mbp.attlocal.net>
2020-12-01 14:05:45 -0500

    user,question,answers migrations done

---
+++ db/migrations/20201201182837-create-questions.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Questions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.STRING(500),
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Questions");
+  },
+};
