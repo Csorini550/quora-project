3262bbceeeeea070a48ae878ca48da875aa82ecf
Dez <dez@dezs-mbp.attlocal.net>
2020-12-03 18:38:56 -0500

    routes and index updates

--- routes/users.js
+++ routes/users.js
@@ -147,6 +147,34 @@ router.post(
   })
 );
 
+// // Create new user
+// router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, res, next) => {
+//   const { email, firstName, lastName, password } = req.body;
+//   const user = db.User.build({
+//     email,
+//     firstName,
+//     lastName,
+//   });
+
+//   const validatorErrors = validationResult(req);
+//   if (validatorErrors.isEmpty()) {
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     user.hashedPassword = hashedPassword;
+//     await user.save();
+//     loginUser(req, res, user);
+//     res.redirect('/');
+//   } else {
+//     const errors = validatorErrors.array().map((error) => error.msg);
+//     res.render('user-register', {
+//       title: 'Register',
+//       user,
+//       errors,
+//       csrfToken: req.csrfToken(),
+//     });
+//   }
+
+// }));
+
 //******************************************************
 //******************** User Login **********************
 //******************************************************
@@ -244,12 +272,75 @@ router.get(
 //******************** Questions ***********************
 //******************************************************
 
-//TODO: QUESTION ROUTES
+router.get(
+  "/users/:id/questions",
+  asyncHandler(async (req, res) => {
+    if (!res.locals.authenticated) {
+      return res.redirect("/login");
+    } else if (parseInt(req.params.id, 10) !== res.locals.user.id) {
+      res.redirect("/");
+    } else {
+      const questions = await db.Question.findAll({
+        where: { userId: res.locals.user.id },
+        include: [
+          { model: User, as: "User", attributes: ["email"] },
+
+          {
+            model: Answer,
+            as: "Answers",
+            attributes: [[Answer.sequelize.fn("COUNT", "id"), "answerCount"]],
+          },
+        ],
+        order: [["createdAt", "DESC"]],
+        attributes: ["value", "createdAt", "id"],
+        group: ["Question.id", "User.id", "Answers.id"],
+      });
+
+      addAnswerCount(questions);
+      convertDate(questions);
+
+      res.render("myQuestion", { questions });
+    }
+  })
+);
 
 //******************************************************
 //******************** Answers *************************
 //******************************************************
 
-//TODO: ANSWER ROUTES
+router.get(
+  "/users/:id/answers",
+  asyncHandler(async (req, res) => {
+    if (!res.locals.authenticated) {
+      return res.redirect("/login");
+    } else if (parseInt(req.params.id, 10) !== res.locals.user.id) {
+      res.redirect("/");
+    } else {
+      const questions = await db.Question.findAll({
+        include: [
+          { model: User, as: "User", attributes: ["email"] },
+
+          {
+            model: Answer,
+            as: "Answers",
+            attributes: [
+              [Answer.sequelize.fn("COUNT", "id"), "answerCount"],
+              "userId",
+            ],
+            where: { userId: res.locals.user.id },
+          },
+        ],
+        order: [["createdAt", "DESC"]],
+        attributes: ["value", "createdAt", "id"],
+        group: ["Question.id", "User.id", "Answers.id"],
+      });
+
+      addAnswerCount(questions);
+      convertDate(questions);
+
+      res.render("myQuestion", { questions });
+    }
+  })
+);
 
 module.exports = router;
