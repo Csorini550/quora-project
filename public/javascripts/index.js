6117ccf08aed1bac6b0f6414e4ef4ab870fd3ce0
matthewhasan <matthewhasan1218@gmail.com>
2020-12-03 18:14:33 -0600

    search bar now works

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event)=>{
     const searchBar = document.querySelector('#search-bar');
+    const searchContainer = document.querySelector('#search-container')
 
     searchBar.addEventListener('submit', async(event) => {
         event.preventDefault();
@@ -14,27 +15,49 @@ window.addEventListener("DOMContentLoaded", (event)=>{
                 body: JSON.stringify(body)
             });
             const foundQuestions = await res.json();
+            console.log(foundQuestions);
 
-            let { questions } = foundQuestions;
+            // let { questions } = foundQuestions;
+
+            let inputText = document.createElement('p');
             
-            if (questions.length === 0) {
-                main.innerHtml = `<p> No results found! </p>`
+            if (foundQuestions.length === 0) {
+                // inputBox = `<p> No results found! </p>`
+
+                inputText.innerHTML = 'No Results Found!'
+                searchContainer.appendChild(inputText);
+
+                //document.innerHTML = `<p> No results found! </p>`
             } else {
-                let questionsHTML = "ul";
-                for (let question of questions) {
-                    questionsHTML += `<a href="/questions/${question.id}/">
-                        <div class="questionBox"> 
-                            <li class="questionBox__content"> ${question.value} </li>
-                        </div>
-                    </a>`;
+
+                //inputText.innerHTML = foundQuestions;
+                
+                for (let question in foundQuestions) {
+                    let list = document.createElement('ul');
+                    let listItem = document.createElement('li');
+                    let questionObj = foundQuestions[question];
+                    let questionValue = questionObj.value;
+                    let valueNode = document.createTextNode(`${questionValue} \n`)
+                    listItem.appendChild(valueNode);
+                    list.appendChild(listItem);
+                    searchContainer.appendChild(list);
                 }
-            questionsHTML += `</ul>`;
-            main.innerHTML = questionsHTML;
+
+
+                // searchContainer.appendChild()
+
+            //     let questionsHTML = "ul";
+            //     for (let question of foundQuestions) {
+            //         questionsHTML += `<a href="/questions/${question.id}/">
+            //             <div class="questionBox"> 
+            //                 <li class="questionBox__content"> ${question.value} </li>
+            //             </div>
+            //         </a>`;
+            //     }
+            // questionsHTML += `</ul>`;
+            // document.innerHTML = questionsHTML;
+
             }
-
-            // foundQuestions.forEach((question) => {
-
-            // });
 
         } catch(err) {
             console.error(err);
