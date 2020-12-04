31a5a8ed683f4ab881cffa22009c1b76ad9070a2
Christopher Sorini <csorini13@gmail.com>
2020-12-04 14:36:30 -0600

    do not push this to mastergit add .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+const requireAuth = (req, res, next) => {
+  if (!res.locals.authenticated) {
+    return res.redirect("/users/login");
+  }
+  return next();
+};
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
+
 });
 
 module.exports = router;
