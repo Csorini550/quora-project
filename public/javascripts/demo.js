c1153a9a71c3bf5f92a94e80f15573bf4d142531
Christopher Sorini <csorini13@gmail.com>
2020-12-06 22:49:12 -0600

    fixing demo user

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -2,7 +2,7 @@ window.addEventListener("load", (e) => {
   const email = document.getElementById("email");
   const password = document.getElementById("password");
   document.getElementById("demo-login").addEventListener("click", (e) => {
-    email.value = "demo@demo.com";
-    password.value = "Password123!";
+    email.value = "demo@user.com";
+    password.value = "Password12!";
   });
 });
