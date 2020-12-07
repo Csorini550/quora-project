9909f13da319b12cdea60b06d9d04dd3136efe5a
Dez <dez@dezs-mbp.attlocal.net>
2020-12-06 22:19:03 -0500

    logo.png add

--- db/seeders/20201201203540-answers.js
+++ db/seeders/20201201203540-answers.js
@@ -1,24 +1,26 @@
-'use strict';
+"use strict";
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Answers', [
-            {
-                value: 'Yea Joe already said we will get an A.',
-                userId: 1,
-                questionId: 1,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            },
-            {
-                value: 'Birds, you idiot!',
-                questionId: 2,
-                userId: 2,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            },
-             {
-                value: `In my GED class, I shared a poem I wrote about my relationship with Math. I called it Me &
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Answers",
+      [
+        {
+          value: "Yea Joe already said we will get an A.",
+          userId: 1,
+          questionId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          value: "Birds, you idiot!",
+          questionId: 2,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          value: `In my GED class, I shared a poem I wrote about my relationship with Math. I called it Me &
                         Math.
                         
                         After sharing the poem—which students enjoyed—students wrote their own poems
@@ -26,14 +28,17 @@ module.exports = {
                         
                         This was an excellent way of getting them to talk
                         about how they were feeling, and for me.`,
-                questionId: 2,
-                userId: 2,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            }], {});
-    },
+          questionId: 2,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Answers', null, {});
-    }
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Answers", null, {});
+  },
 };
